c556867755c404c888c60f31486cf3d763cc21fe
Prince <veluthedathuprince@gmail.com>
2023-05-04 13:11:34 +0200

    Still fixing errors

--- server/api/api.ts
+++ server/api/api.ts
@@ -1,4 +1,8 @@
 import { Request, Response, Express } from 'express'
+import { Database } from '../database'
+
+
+const db = new Database();
 
 export class API {
   // Properties
@@ -6,10 +10,42 @@ export class API {
   // Constructor
   constructor(app: Express) {
     this.app = app
-    this.app.get('/hello', this.sayHello)
+    this.app.get('/login', this.sayHello.bind(this));
+    this.app.post('/tweets', this.tweets.bind(this));
+    
+  }
+  private async tweets (req: Request, res: Response){
+
+    const user_id: number = 1;
+    const content: string = 'erth';
+    const likes: number = 1;
+    const dislike: number = 1;
+    
+    const query = `INSERT INTO tweets (user_id, content, likes, dislike) VALUES (?, ?, ?, ?)`;
+    const values: (string | number)[] = [user_id, content, likes, dislike];
+    
+    await db.executeSQL(values);
+    await db.executeSQL(query);
+        res.send(result)
+    
   }
   // Methods
   private sayHello(req: Request, res: Response) {
     res.send('Hello There!')
-  }
+  } 
+  private async postTweet(req: Request, res: Response) {
+      const { user_id, content, likes, dislike } = req.body
+        
+
+      
+
+
+  }}
+
+
+
+
+function result(result: any) {
+  throw new Error('Function not implemented.');
 }
+
